f90b548159984ef8bbea92fb381fd7b3eb294481
TSITCL\s162190 <kilito.shuto.bb@gmail.com>
2017-12-14 16:10:51 +0900

    UIを実装、調整

--- Assets/Editor/ReadOnlyDrawer.cs
+++ Assets/Editor/ReadOnlyDrawer.cs
@@ -3,18 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-// Inspector display setting
 [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
-// PropertyDrawer -> 
 public class ReadOnlyDrawer : PropertyDrawer
 {
-    // Update display processing
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
-        // not editor for Insoctor
         GUI.enabled = false;
-        // 指定した領域に指定した変数を描画
         EditorGUI.PropertyField(position, property, label, true);
-       GUI.enabled = true;
+        GUI.enabled = true;
     }
 }
---
+++ Assets/Script/AttributeTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttributeTest : MonoBehaviour
+{
+    // Draw and edit private member as Inspector
+    [SerializeField]
+    private int private_pram_show;
+    private int private_pram;
+
+    // Do not Draw and edit public member as Inspector
+    [HideInInspector]
+    public int public_parm_hide;
+    public int public_parm;
+
+    // Draw public class as Inspector
+    [System.Serializable]
+    public class PublicClassShow
+    {
+        public int param;
+    }
+    public PublicClassShow public_class;
+    public class NormalClass
+    {
+        public int param;
+    }
+    public NormalClass normal_class;
+
+    [ReadOnly]
+    private int param;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
